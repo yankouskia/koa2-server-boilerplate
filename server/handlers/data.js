d6b8eb3f12dacbbd06378f581f6e95d597868e74
Michael J. Ryan <tracker1@gmail.com>
2016-10-29 02:47:53 -0700

    Update data.js
    Await on Promise.resolve is superfluous.

--- server/handlers/data.js
+++ server/handlers/data.js
@@ -4,11 +4,11 @@ import { getItem, setItem } from 'utils/serverStorage';
 
 export async function load(ctx) {
   const data = await getItem('data');
-  ctx.body = await Promise.resolve({ data });
+  ctx.body = { data };
 };
 
 export async function save(ctx) {
   const data = ctx.request.body;
   await setItem('data', data);
-  ctx.body = await Promise.resolve({ status: 'ok' });
+  ctx.body = { status: 'ok' };
 };
